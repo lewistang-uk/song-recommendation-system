c7138ef08e9039b5d544b6f623241386d557ba20
lewistang-uk <ltang302@gmail.com>
2026-07-14 12:21:35 +0100

    updated streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,10 +7,11 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parent.parent
 DATA = ROOT / "data"
 
-lyrics_df = pd.read_csv(DATA / 'lyrics_transformed.csv').drop('Unnamed: 0', axis=1)
-audio_df = pd.read_csv(DATA / 'audio_features_transformed.csv').drop('Unnamed: 0', axis=1)
 D_lyrics_df = pd.read_csv(DATA / 'lyrics_dissimilarities.csv')
 D_audio_df = pd.read_csv(DATA / 'audio_dissimilarities.csv')
+name_to_id = pd.read_csv(DATA / 'name_to_id.csv').set_index('name')
+id_to_name = name_to_id.reset_index().set_index("id")
+
 
 # cast index/column names to numbers
 D_audio_df.index = D_audio_df.index.astype(int)
@@ -19,61 +20,14 @@ D_audio_df.columns = D_audio_df.columns.astype(int)
 D_lyrics_df.index = D_lyrics_df.index.astype(int)
 D_lyrics_df.columns = D_lyrics_df.columns.astype(int)
 
-# string cleaning
-def transform_name(name):
-    # get rid of spaces
-    name = re.sub(r" ", "", name)
-
-    # get rid of apostrophes and +
-    name = re.sub(r"['’‘+\"]", "", name)
-
-    # change punctuation to underscore
-    name = re.sub(r'[()?.,\-!&]', "_", name)
-
-    # lowercase all
-    name = name.lower()
-
-    # remove 'bonus track'
-    name = re.sub(r"_bonustrack", "", name)
-    
-    # remove features
-    name = re.sub(r'_feat_[a-z]+_', "", name)
-
-    # final exceptions
-    name = re.sub(r"popversion_", "popversion", name)
-    name = re.sub(r"atthedisco_", "", name)
-    
-    return name
-
-def normalise_apostrophes(name):
-    return re.sub(r"[\"’‘]", "'", name)
-
-# for search bar
-list_of_names = [normalise_apostrophes(name) for name in audio_df['name']]
-list_of_names.pop(list_of_names.index('Teardrops On My Guitar - Radio Single Remix'))
-
-# make a hashmap for names and ids
-names_df = pd.DataFrame({'raw': list_of_names, 'name': [transform_name(name) for name in list_of_names]})
-
-audio_df['name'] = audio_df['name'].apply(transform_name)
-lyrics_df['name'] = lyrics_df['name'].apply(transform_name)
-df = audio_df.merge(lyrics_df, on='name')
-df['id'] = [i for i in range(len(df))]
-
-id_df = df[['id', 'name']]
-names_df = names_df.merge(id_df, on='name').drop('name', axis=1)
-
-order = [(names_df['raw'][i], i) for i in range(len(names_df))]
-name_to_id = dict(order)
-id_to_name = dict([(j, i) for (i, j) in order])
 
 # recommendation algorithms - k-NN based
 def recommend(query_name, alpha=0.5, k=5):
     D = alpha * D_audio_df + (1 - alpha) * D_lyrics_df
-    query_id = name_to_id[query_name]
+    query_id = name_to_id.loc[query_name, 'id']
     distances = D.loc[query_id].copy()
     distances.loc[query_id] = np.inf  # don't recommend itself
-    return [id_to_name[int(x)] for x in distances.nsmallest(k).index]
+    return [id_to_name.loc[x]['name'] for x in distances.nsmallest(k).index]
 
 def recommend_weighted(query_name: str, prev_queries: list, alpha=0.5, k=5):
     """Takes query and a list of previous queries (ordered, most recent first)"""
@@ -86,7 +40,7 @@ def recommend_weighted(query_name: str, prev_queries: list, alpha=0.5, k=5):
     else:
         s_current = 1
 
-    query_id = name_to_id[query_name]
+    query_id = name_to_id.loc[query_name, 'id']
     distances = D.loc[query_id].copy()*s_current
     distances.loc[query_id] = np.inf  # don't recommend itself
 
@@ -95,11 +49,11 @@ def recommend_weighted(query_name: str, prev_queries: list, alpha=0.5, k=5):
         return (2*(S-i))/(5*S*(S+1))
         
     for i, query in enumerate(prev_queries):
-        query_id = name_to_id[query]
+        query_id = name_to_id.loc[query, 'id']
         query_distances = D.loc[query_id].copy() * W(i, len(prev_queries))
-        query_distances.loc[query_id] += 0.5/(i+1) # make it less likely to recommend something already searched 
+        query_distances.loc[query_id] += 1/(i+1) # make it less likely to recommend something already searched 
         distances = distances + query_distances
-    return [id_to_name[int(x)] for x in distances.nsmallest(k).index]
+    return [id_to_name.loc[x]['name'] for x in distances.nsmallest(k).index]
 
 def search(query_name: str, prev_queries: list, balance='equal', use_prev=True, k=5):
     """
@@ -144,7 +98,7 @@ with st.sidebar:
             st.markdown(f"- {item}")
 
 # main search bar
-selection = st.selectbox('Choose a song that you liked:', list_of_names)
+selection = st.selectbox('Choose a song that you liked:', list(name_to_id.index))
 
 # show results
 if st.button('Search'):
@@ -162,4 +116,3 @@ if st.button('Search'):
     "Recommendation": recommendations
     })
     st.dataframe(df, width='stretch', hide_index=True)
-    
